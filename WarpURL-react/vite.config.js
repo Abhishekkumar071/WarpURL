70a2ad0e48e0186517c2b9818336daa363bc111b
Abhishek Kumar <abhishekkumarbgp0917@gmail.com>
2026-05-17 01:58:05 +0530

    Make Changes in API URL

--- WarpURL-react/vite.config.js
+++ WarpURL-react/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 export default defineConfig({
   plugins: [react()],
@@ -11,7 +12,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: baseURL,
         changeOrigin: true,
       },
     },
